a682c142d2738b13dc359b4c5fa742c3d0626d9d
dorgrosman <71778859+dorgrosman@users.noreply.github.com>
2026-06-20 23:46:10 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,21 +1,3 @@
-const CACHE_NAME = 'family-waabit-v2';
-
-self.addEventListener('install', e => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-// Network-first, no aggressive caching that could break installs
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
-});
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', e => {});
